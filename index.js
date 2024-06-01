d611035de2cd25af11ec2106a2149245f3a4eb56
Kayla Little <kaylalittle.dev@gmail.com>
2024-06-01 14:46:26 -0400

    updated the contact form, page, and css and JS. Need to update the template code in JS but awaiting client's feedback for that.

--- index.js
+++ index.js
@@ -6,3 +6,27 @@ function openMenu() {
 function closeMenu() {
     document.body.classList.remove('menu--open')
 }
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".temp__overlay--loading");
+  const success = document.querySelector(".temp__overlay--success");
+  loading.classList += " temp__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_sqmgemo",
+      "template_d9refyl",
+      event.target,
+      "aXlVnZVZjkjMTnpmw"
+    )
+    .then(() => {
+      loading.classList.remove("temp__overlay--visible");
+      success.classList += " temp__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("temp__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly at nstephens@koomson.co or call us at 813-323-1954"
+      );
+    });
+  }
